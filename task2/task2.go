880a9a4ba991c4d8a89988bfb26cf47a5e0f2ead
加了盐的咖啡 <605823850@qq.com>
2025-10-27 00:38:13 +0800

    task2

--- task2/task2.go
+++ task2/task2.go
@@ -169,6 +169,22 @@ func (c Circle) Area() float64 {
 func (c Circle) Perimeter() float64 {
 	return 2 * math.Pi * c.Radius
 }
+
+// 题目 ：使用组合的方式创建一个 Person 结构体，包含 Name 和 Age 字段，再创建一个 Employee 结构体，
+// 组合 Person 结构体并添加 EmployeeID 字段。为 Employee 结构体实现一个 PrintInfo() 方法，输出员工的信息。
+// 考察点 ：组合的使用、方法接收者。
+type Person struct {
+	Name string
+	Age  int
+}
+type Employee struct {
+	Person     // 匿名嵌入 Person，实现组合（字段和方法自动提升）
+	EmployeeID string
+}
+
+func (e Employee) PrintInfo() string {
+	return fmt.Sprintf("Name: %s, Age: %d", e.Name, e.Age)
+}
 func main() {
 
 	// 题目1
@@ -260,4 +276,15 @@ func main() {
 		fmt.Println("------")
 
 	}
+
+	// 题目 6
+	// 创建 Employee 实例
+	emp := Employee{
+		Person: Person{
+			Name: "张三",
+			Age:  30,
+		},
+		EmployeeID: "111",
+	}
+	emp.PrintInfo()
 }
