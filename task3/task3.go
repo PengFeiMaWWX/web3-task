5a1f0114a0ef431987fc804f3efbd0ee24421637
加了盐的咖啡 <605823850@qq.com>
2025-11-13 22:35:01 +0800

    task2

--- task3/task3.go
+++ task3/task3.go
@@ -83,6 +83,28 @@ func (s *studentService) remove(id int64) error {
 	}
 	return nil
 }
+
+/*
+*
+题目2：事务语句
+假设有两个表：
+accounts 表（包含字段 id 主键， balance 账户余额）和
+transactions 表（包含字段 id 主键， from_account_id 转出账户ID， to_account_id 转入账户ID， amount 转账金额）。
+要求 ：
+编写一个事务，实现从账户 A 向账户 B 转账 100 元的操作。在事务中，
+需要先检查账户 A 的余额是否足够，如果足够则从账户 A 扣除 100 元，向账户 B 增加 100 元，
+并在 transactions 表中记录该笔转账信息。如果余额不足，则回滚事务
+*/
+type Account struct {
+	ID      uint `gorm:"primarykey"` // 无符号（只能表示非负数，即0和正数）
+	Balance int  `gorm:"not null"`   // 有符号（可以表示负数、零和正数）
+}
+
+type Transaction struct {
+	ID            uint  `gorm:"primarykey"`
+	FromAccountId int64 `gorm:"not null"`
+}
+
 func main() {
 
 	studentService, err := NewStudentService()
